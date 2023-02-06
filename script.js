d8c0ede044ceb82c147d42b477934df98d92365e
ck-lamib <Groot@ck-lamibb.local>
2023-02-06 20:53:08 +0545

    rough commit

--- script.js
+++ script.js
@@ -1,47 +1,99 @@
-$(document).ready(function()
-{
-
-   
-        if($('.bbb_viewed_slider').length)
-        {
-            var viewedSlider = $('.bbb_viewed_slider');
-
-            viewedSlider.owlCarousel(
-            {
-                loop:true,
-                margin:30,
-                autoplay:true,
-                autoplayTimeout:6000,
-                nav:false,
-                dots:false,
-                responsive:
-                {
-                    0:{items:1},
-                    575:{items:2},
-                    768:{items:3},
-                    991:{items:4},
-                    1199:{items:6}
-                }
-            });
-
-            if($('.bbb_viewed_prev').length)
-            {
-                var prev = $('.bbb_viewed_prev');
-                prev.on('click', function()
-                {
-                    viewedSlider.trigger('prev.owl.carousel');
-                });
-            }
-
-            if($('.bbb_viewed_next').length)
-            {
-                var next = $('.bbb_viewed_next');
-                next.on('click', function()
-                {
-                    viewedSlider.trigger('next.owl.carousel');
-                });
-            }
-        }
+// var multipleCardCarousel = document.querySelector(
+//     "#carouselExampleControls"
+//   );
+//   if (window.matchMedia("(min-width: 3000px)").matches) {
+//     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+//       interval: false,
+//     });
+//     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+//     var cardWidth = $(".carousel-item").width();
+//     var scrollPosition = 0;
+//     $("#carouselExampleControls .carousel-control-next").on("click", function () {
+//       if (scrollPosition < carouselWidth - cardWidth * 4) {
+//         scrollPosition += cardWidth;
+//         $("#carouselExampleControls .carousel-inner").animate(
+//           { scrollLeft: scrollPosition },
+//           600
+//         );
+//       }
+//     });
+//     $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+//       if (scrollPosition > 0) {
+//         scrollPosition -= cardWidth;
+//         $("#carouselExampleControls .carousel-inner").animate(
+//           { scrollLeft: scrollPosition },
+//           600
+//         );
+//       }
+//     });
+//   } else {
+//     $(multipleCardCarousel).addClass("slide");
+//   }
 
 
-    });
+
+// buttons
+
+var sliderControl = document.querySelector(".slider-control");
+
+// slides informations
+
+var slides = document.querySelectorAll(".carousel-item"),
+    slidesLength = slides.length;
+
+// slides array
+
+var slidesArr = [].slice.call(slides);
+
+// reverse array sorting
+
+slidesArr = slidesArr.reverse();
+
+
+// slide current
+var slideCurrent = 0;
+
+sliderControl.addEventListener("click", function(e){
+  target = e.target;
+
+  // Get next button
+  if(target.classList.contains("next")){
+    next = e.target,
+    prev = next.previousElementSibling,
+    nextSlide = slidesArr[slideCurrent + 1],
+    slide = slidesArr[slideCurrent];
+
+    slide.classList.add("slide-on");
+    slide.classList.remove("text-on");
+    nextSlide.classList.add("text-on");
+
+    slideCurrent += 1;        
+    if(slideCurrent > 0) {
+      prev.classList.remove("disabled");
+    }         
+    if(slideCurrent === slidesLength - 1){
+      next.classList.add("disabled");
+    }
+}     
+
+// get prev button
+   if(target.classList.contains("prev")){
+
+    slideCurrent -= 1;
+
+    prev = e.target,
+    next = prev.nextElementSibling,
+    prevSlide = slidesArr[slideCurrent + 1],
+    slidesArr[slideCurrent];
+    prevSlide.classList.remove("text-on");
+    slide.classList.remove("slide-on");
+    slide.classList.add("text-on");
+
+    if(slideCurrent === slidesLength - 2){
+      next.classList.add("disabled");
+    }
+    if(slideCurrent === 0){
+    prev.classList.add("disabled");}
+
+   }        
+});
